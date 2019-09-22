b8ffe7e46000adc898b80e3b442d55f50501072f
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-09-22 18:17:58 +0300

    add route /messages

--- server.py
+++ server.py
@@ -1,22 +1,32 @@
 from flask import Flask
 from datetime import datetime
+import time
 
 app = Flask(__name__)
+messages = [
+    {"username": "Jack", "time": time.time(), "text": "Hello"},
+    {"username": "Jane", "time": time.time(), "text": "Hi, Jack"},
+]
 
 
 @app.route("/")
-def hello():
+def hello_view():
     return "Hello, World!"
 
 
 @app.route("/status")
-def status():
+def status_view():
     return {
         "status": True,
         # "time": datetime.now()
         # "time": str(datetime.now())
         "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     }
+
+
+@app.route("/messages")
+def messages_view():
+    return {"messages": messages}
 
 
 if __name__ == "__main__":
