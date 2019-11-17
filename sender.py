687666c8582b44b32b7b75bc1dc4ab7e87af62b0
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-11-17 19:55:24 +0300

    fix login

--- sender.py
+++ sender.py
@@ -6,11 +6,11 @@ print(r.json())
 # логинимся
 username = "Ivan"
 password = "111"
-data = {
+login_data = {
     "username": username,
     "password": password
 }
-r = requests.post("http://localhost:5000/login", json=data)
+r = requests.post("http://localhost:5000/login", json=login_data)
 print(r.json())
 
 # отправляем сообщение
@@ -21,5 +21,7 @@ while True:
         "text": text,
         "password": password
     }
+    # логинимся перед каждым запросом, т.к. пользователи у нас хранятся по сути в памяти, и после перезагрузки сервера, наш users сбрасывается в начальное состояние, это решается с помощью бд
+    requests.post("http://localhost:5000/login", json=login_data)
     r = requests.post("http://localhost:5000/send", json=data)
     print(r.json())
